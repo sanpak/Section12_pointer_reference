7140e9ec9aec487258accec021619c7f63d1cf86
Sanpak Ng <>
2021-04-14 11:30:33 +0000

    reference example

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@
 using namespace std;
 
 int main() {
+ int num{10};
+ cout << "Value of num is: " << num << endl;
+ cout << "sizeof num is: " << sizeof num << endl;
+ cout << "Address of num is: " << &num << endl;
  
+ return 0;
 }
